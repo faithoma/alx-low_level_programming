35a0e7d455cc89764ccabe6cbca1aab528505f19
faithoma <faithoma23@gmail.com>
2022-09-15 17:30:15 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			printf("FizzBuzz ");
+			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
