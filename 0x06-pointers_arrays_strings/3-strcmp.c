df9a6e1df15ccbb1a6f89855ba856aaaf1b84cde
faithoma <faithoma23@gmail.com>
2022-09-21 10:02:24 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strcmp - compares two strings
- * A pointer to a character that will be changed
+ * @s1: A pointer to a character that will be changed
  * @s2: A pointer to a character that will also be changed/modified/updated
  * Return: dest
  */
@@ -9,6 +9,7 @@
 int _strcmp(char *s1, char *s2)
 
 {
+
 char *str_one = s1;
 char *str_two = s2;
 
